bd73e48375bfdaddb0efba8c8eb8414b569d4a10
GULZHAS MAILYBAYEVA <gulzhas.mailybayeva@tvh.com>
2023-11-07 11:16:18 +0600

    circular singly list operations

--- src/main/java/linked_list/circular_singly/Main.java
+++ src/main/java/linked_list/circular_singly/Main.java
@@ -3,7 +3,15 @@ package linked_list.circular_singly;
 public class Main {
     public static void main(String[] args) {
         CircularSinglyLinkedList csll = new CircularSinglyLinkedList();
-        csll.createCSLL(7);
+        csll.createCSLL(5);
+        csll.insertCSLL(4, 2);
+        csll.insertCSLL(6, 1);
+        csll.insertCSLL(7, 8);
         System.out.println(csll.head.value);
+        csll.traverseCSLL();
+        csll.deleteNode(7);
+        csll.traverseCSLL();
+        csll.deleteCSLL();
+        csll.traverseCSLL();
     }
 }
