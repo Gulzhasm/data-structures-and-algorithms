ac383c6965abbfdc4d511def57a9579ded8e168f
Gulzhasm <gulzhasm@gmail.com>
2023-11-16 12:42:30 +0600

    exercises

--- src/main/java/bootcamp/intro/Calculator.java
+++ src/main/java/bootcamp/intro/Calculator.java
@@ -1,5 +1,13 @@
 package bootcamp.intro;
 
-public class Calculator {
+import java.util.Scanner;
 
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        //take input from a user until the user presses X
+
+
+    }
 }
