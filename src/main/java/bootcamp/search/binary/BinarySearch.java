1569ab7448ea871b7cc15fc36e9a9705818b0a08
Gulzhasm <gulzhasm@gmail.com>
2023-11-19 16:54:55 +0600

    binary search

---
+++ src/main/java/bootcamp/search/binary/BinarySearch.java
@@ -0,0 +1,28 @@
+package bootcamp.search.binary;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int target = binarySearch(new int[]{4, 5, 6, 343, 4343, 45454}, 4);
+        System.out.println(target);
+    }
+
+    //return the index
+    static int binarySearch(int[] arr, int target) {
+        if (arr.length == 0) return -1;
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            //find the middle
+            int midX = start + (end - start) / 2;
+            if (target < arr[midX]) {
+                end = midX - 1;
+            } else if (target > arr[midX]) {
+                start = midX + 1;
+            } else {
+                return midX;
+            }
+        }
+        return -1;
+    }
+}
