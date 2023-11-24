9697e8c7d01ed1dd69525fc0ac4e027b7337c203
Gulzhasm <gulzhasm@gmail.com>
2023-11-24 10:51:23 +0600

    separate leetcode from bootcamp folder

---
+++ src/main/java/leetcode/leetcode/Concentration.java
@@ -0,0 +1,22 @@
+package leetcode.leetcode;
+
+import java.util.Arrays;
+
+public class Concentration {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3};
+        System.out.println(Arrays.toString(getConcentration(arr)));
+    }
+
+    static int[] getConcentration(int[] arr) {
+
+        int n = arr.length;
+        int[] output = new int[2 * n];
+        for (int i = 0; i < arr.length; i++) {
+            output[i] = arr[i];
+            output[n] = arr[i];
+            n++;
+        }
+        return output;
+    }
+}
---
+++ src/main/java/leetcode/leetcode/ShuffleArray.java
@@ -0,0 +1,30 @@
+package leetcode.leetcode;
+
+import java.util.Arrays;
+
+public class ShuffleArray {
+    public static void main(String[] args) {
+        int[] arr = {1,1,2,2}; // 2 3 5 4 1 7
+        System.out.println(Arrays.toString(shuffle(arr, 2)));
+    }
+
+    static int[] shuffle(int[] nums, int n) {
+
+        int temp = 0;
+        int temp2 = 0;
+
+        for (int i = 1; i < nums.length / 2; i++) {
+            temp = nums[2 * i - 1];
+            temp2 = nums[i + 1];
+
+            nums[2 * i - 1] = nums[n];
+            nums[i + 1] = temp;
+
+            nums[n] = nums[n + 1];
+            nums[n + 1] = temp2;
+
+        }
+
+        return nums;
+    }
+}
