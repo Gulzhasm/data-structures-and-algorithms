53c58d1ff45791596bd097d1ee6d4d094f4854e5
Gulzhasm <gulzhasm@gmail.com>
2023-11-26 19:02:58 +0600

    circular list

--- src/main/java/bootcamp/linkedLists/Main.java
+++ src/main/java/bootcamp/linkedLists/Main.java
@@ -2,7 +2,7 @@ package bootcamp.linkedLists;
 
 public class Main {
     public static void main(String[] args) {
-        LL ll = new LL();
+      /*  LL ll = new LL();
         ll.insertFirst(3);
         ll.insertFirst(2);
         ll.insertFirst(8);
@@ -13,5 +13,29 @@ public class Main {
         System.out.println();
         System.out.println(ll.deleteAtIndex(2));
         ll.display();
+
+
+
+        DLL dll  = new DLL();
+        dll.insertFirst(3);
+        dll.insertFirst(2);
+        dll.insertFirst(8);
+        dll.insertFirst(17);
+        dll.display();
+        dll.insertLast(99);
+        dll.display();
+        dll.insertAtIndex(99,65);
+        dll.display();
+        */
+        CLL cll  = new CLL();
+        cll.insert(3);
+        cll.insert(2);
+        cll.insert(8);
+        cll.insert(17);
+        cll.display();
+        cll.delete(8);
+        cll.display();
+
+
     }
 }
