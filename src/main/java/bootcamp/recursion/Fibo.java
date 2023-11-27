069cd11410f7b8ca249fc83412dd9af919acbca0
Gulzhasm <gulzhasm@gmail.com>
2023-11-27 10:54:36 +0600

    recursion intro

---
+++ src/main/java/bootcamp/recursion/Fibo.java
@@ -0,0 +1,15 @@
+package bootcamp.recursion;
+
+public class Fibo {
+    public static void main(String[] args) {
+        System.out.println(fibo(50)); //
+    }
+
+    static int fibo(int n) { //golden ratio is O(n^2), but the number increases, for ex: 50, time complexity is exponentially increases, very very very bad :)
+        //base condition
+        if (n < 2) {
+            return n;
+        }
+        return fibo(n - 1) + fibo(n - 2);
+    }
+}
---
+++ src/main/java/bootcamp/recursion/Message.java
@@ -0,0 +1,11 @@
+package bootcamp.recursion;
+
+public class Message {
+    public static void main(String[] args) {
+        message();
+    }
+
+     static void message() {
+
+    }
+}
