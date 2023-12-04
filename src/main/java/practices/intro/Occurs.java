9e47b892b7297a7fbcf4a0286a0ebfde0a631bba
Gulzhasm <gulzhasm@gmail.com>
2023-12-04 19:56:50 +0600

    some leetcode solutions

--- src/main/java/practices/intro/Occurs.java
+++ src/main/java/practices/intro/Occurs.java
@@ -1,17 +1,76 @@
 package practices.intro;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Occurs {
     public static void main(String[] args) {
+    String[] nums = {"abc","bcd","aaaa","cbc"};
+        System.out.println(findWordsContaining(nums, 'a'));
+    }
+    public static  List<Integer> findWordsContaining(String[] words, char x) {
+        List<Integer> ans = new ArrayList<>();
+        for(int i = 0; i < words.length; i++){
+            for(char ch : words[i].toCharArray()){
+                if(ch == x){
+                    ans.add(i);
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+
+
+    public static int findNumbers(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 9 && nums[i] % 2 == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    static void bubble(int[] arr) {
+        boolean swapped;
+        for (int i = 0; i < arr.length; i++) {
+            swapped = false;
+            for (int j = 1; j < arr.length - i; j++) {
+                if (arr[j] < arr[j - 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+
+
+    public static int[] sortedSquares(int[] nums) {
+        for(int i = 0; i < nums.length; i++){
+            nums[i] = Math.abs(nums[i] * nums[i]);
+        }
+        bubble(nums);
+        return nums;
+    }
+
+    void occur(int num){
         // n = 12343 how many times occurs 3
         int n = 1234333333;
         int count = 0;
 
-        while(n > 0){
+        while (n > 0) {
             int rem = n % 10;
-            if(rem == 3){
+            if (rem == 3) {
                 count++;
             }
-            n = n/10;
+            n = n / 10;
         }
         System.out.println(count);
     }
--- src/main/java/practices/oop/abstractDemo/interfaces/Engine.java
+++ src/main/java/practices/oop/abstractDemo/interfaces/Engine.java
@@ -1,7 +1,6 @@
 package practices.oop.abstractDemo.interfaces;
 
 public interface Engine {
-    static final int PRICE = 78000;
 
     void start();
     void stop();
