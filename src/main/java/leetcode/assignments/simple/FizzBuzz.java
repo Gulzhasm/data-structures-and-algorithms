3ca7ffdbe773c8fb405caa9684131791637d9ada
Gulzhasm <gulzhasm@gmail.com>
2023-12-12 20:32:00 +0600

    some easy leetcode

--- src/main/java/leetcode/assignments/simple/FizzBuzz.java
+++ src/main/java/leetcode/assignments/simple/FizzBuzz.java
@@ -7,8 +7,38 @@ import java.util.List;
 public class FizzBuzz {
 
     public static void main(String[] args) {
-        System.out.println(isPalindrome("`l;``1o1??;l`"));
-                reverseString(new char[] {'h', 'e', 'l', 'l', 'o'});
+        System.out.println(mySqrt(8));
+    }
+
+    public int firstBadVersion(int n) {
+        int start = 0;
+        int end = n;
+        while(start <= end){
+            int mid = start + (end -start)/2;
+            if(isBadVersion(mid) == true && isBadVersion(mid-1) == false) return mid;
+            else if(isBadVersion(mid) == false) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    private boolean isBadVersion(int mid) {
+        int n = 0;
+        return mid == n;
+    }
+
+    public static int mySqrt(int x) {
+        int n = 1;
+        int count = 0;
+        while(x > 0) {
+            x = x - n;
+            n = n + 2;
+            count++;
+        }
+        return count;
     }
 
     public static  boolean isPalindrome(String s) {
