f58c4fe801d34698a139017be94783d2f6759323
Gulzhasm <gulzhasm@gmail.com>
2023-12-31 11:27:41 +0600

    daily leetcode

--- src/main/java/algorithms/bfs/Solution.java
+++ src/main/java/algorithms/bfs/Solution.java
@@ -26,6 +26,68 @@ class TreeNode {
 }
 
 class Solution {
+    //Input: root = [3,9,20,null,null,15,7]
+//Output: [3.00000,14.50000,11.00000]
+//Explanation: The average value of nodes on level 0 is 3, on level 1 is 14.5, and on level 2 is 11.
+//Hence return [3, 14.5, 11].
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            double average = 0;
+
+            for (int i = 0; i < size; i++) {
+                TreeNode currentNode = queue.poll();
+                average += currentNode.val;
+
+                if (currentNode.left != null) {
+                    queue.offer(currentNode.left);
+                }
+
+                if (currentNode.right != null) {
+                    queue.offer(currentNode.right);
+                }
+            }
+            average = average / size;
+            result.add(average);
+        }
+            return result;
+    }
+
+    public List<List<Integer>> levelOrder1(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> currentLevel = new ArrayList<>(size);
+            for (int i = 0; i < size; i++) {
+                TreeNode currentNode = queue.poll();
+                currentLevel.add(currentNode.val);
+                if (currentNode.left != null) {
+                    queue.add(currentNode.left);
+                }
+                if (currentNode.right != null) {
+                    queue.add(currentNode.right);
+                }
+            }
+            result.add(currentLevel);
+        }
+        return result;
+    }
+
+
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> result = new ArrayList<>();
         Queue<TreeNode> queue = new LinkedList<>();
