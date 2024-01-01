40b92926acafe4996b31f44214c1217c556c70ee
Gulzhasm <gulzhasm@gmail.com>
2024-01-01 14:34:52 +0600

    bfs recap

--- src/main/java/algorithms/bfs/LevelOrderTraversal.java
+++ src/main/java/algorithms/bfs/LevelOrderTraversal.java
@@ -1,7 +1,30 @@
 package algorithms.bfs;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class LevelOrderTraversal {
     public static void main(String[] args) {
         //
+    }
+
+    static void bfs(TreeNode root){
+        if(root == null){
+            return;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while(!queue.isEmpty()){
+            TreeNode currentNode = queue.poll();
+            System.out.println(currentNode.val);
+            if(currentNode.left != null){
+                queue.offer(currentNode.left);
+            }
+            if(currentNode.right != null){
+                queue.offer(currentNode.right);
+            }
+        }
     }
 }
