01ba57d69872d1342e89ed4122e4743b05033c81
Gulzhasm <gulzhasm@gmail.com>
2024-01-02 10:08:03 +0600

    binary tree bfs recap

---
+++ src/main/java/algorithms/bfs/GoogleDriveEx.java
@@ -0,0 +1,57 @@
+package algorithms.bfs;
+
+import java.util.*;
+
+public class GoogleDriveEx {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            List<Integer> currentLevel = new ArrayList<>(levelSize);
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode node = queue.poll();
+                currentLevel.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            result.add(currentLevel);
+        }
+        return result;
+    }
+
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> result = new ArrayList<>();
+        if (root == null) return result;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            double average = 0;
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode node = queue.poll();
+                {
+                    average += node.val;
+                    if (node.left != null) {
+                        queue.offer(node.left);
+                    }
+                    if (node.right != null) {
+                        queue.offer(node.right);
+                    }
+                }
+            }
+            average = average / levelSize;
+            result.add(average);
+        }
+        return result;
+    }
+}
