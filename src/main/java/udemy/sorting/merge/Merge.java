a7ac4e04a68c77fed3474ba51a4fc783ef9f92ed
Gulzhasm <gulzhasm@gmail.com>
2024-01-04 10:30:52 +0600

    bucket sort

---
+++ src/main/java/udemy/sorting/merge/Merge.java
@@ -0,0 +1,10 @@
+package udemy.sorting.merge;
+
+public class Merge {
+   //TODO learn merge and quick sorts thoroughly and then implement the algorithms
+    public void printArray(int[] arr){
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
