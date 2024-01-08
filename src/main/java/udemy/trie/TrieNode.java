f8ad1cd735c8eaf3587d232d08c9d0b1fcb6d297
Gulzhasm <gulzhasm@gmail.com>
2024-01-08 10:43:07 +0600

    trie implementation with hash map

---
+++ src/main/java/udemy/trie/TrieNode.java
@@ -0,0 +1,16 @@
+package udemy.trie;
+
+import java.util.Map;
+import java.util.HashMap;
+
+
+public class TrieNode {
+
+    Map<Character, TrieNode> children;
+    boolean endOfString;
+
+    public TrieNode() {
+        children = new HashMap<>();
+        endOfString = false;
+    }
+}
