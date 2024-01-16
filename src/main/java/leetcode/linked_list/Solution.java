86728eef6a4c4734d250a48f9041c61853b01ccc
Gulzhasm <gulzhasm@gmail.com>
2024-01-16 10:08:09 +0600

    linked list review

---
+++ src/main/java/leetcode/linked_list/Solution.java
@@ -0,0 +1,87 @@
+package leetcode.linked_list;
+
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class Solution {
+
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int countCycles(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (fast == slow) {
+                ListNode temp = slow;
+                int count = 0;
+                do {
+                    temp = temp.next;
+                    count++;
+                } while (temp != slow);
+
+                return count;
+            }
+        }
+
+        return 0;
+    }
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode ans = new ListNode();
+        ListNode node = ans;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                node.next = list1;
+                list1 = list1.next;
+            } else {
+                node.next = list2;
+                list2 = list2.next;
+            }
+            node = node.next;
+        }
+        node.next = (list1 != null) ? list1 : list2;
+        return ans.next;
+    }
+
+    public ListNode deleteDuplicates(ListNode node) {
+        ListNode head = node;
+        while (node.next != null) {
+            if (node.val == node.next.val) {
+                node.next = node.next.next;
+            } else {
+                node = node.next;
+            }
+        }
+        return head;
+    }
+}
