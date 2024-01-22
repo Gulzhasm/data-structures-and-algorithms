a481c15e7bad6cdd19412116169f8b305b8a3773
Gulzhasm <gulzhasm@gmail.com>
2024-01-22 15:40:01 +0600

    daily leetcode challenge

---
+++ src/main/java/leetcode/leetcode/daily/january/Jan22.java
@@ -0,0 +1,59 @@
+package leetcode.leetcode.daily.january;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Jan22 {
+
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int i = 0;
+        for(int j = 0; i < g.length && j < s.length; j++){
+            if(s[j] >= g[i]){
+                i++;
+            }
+        }
+        return i;
+
+    }
+    //Input: nums = [-1,0,1,2,-1,-4]
+    //Output: [[-1,-1,2],[-1,0,1]]
+    public List<List<Integer>> threeSum(int[] a) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(a);
+
+        for(int i = 0; i < a.length; i++){
+            for(int j = i+1; j <a.length; j++) {
+                List<Integer> current = new ArrayList<>();
+                int k = bs(a, -(a[i] + a[j]));
+                if(k != -1){
+                    if((a[i] + a[j]) + a[k] == 0){
+                        current.add(a[i]);
+                        current.add(a[j]);
+                        current.add(a[k]);
+                    }
+                }
+                ans.add(current);
+            }
+        }
+        return ans;
+    }
+
+    private int bs(int[] nums, int key){
+        int lo = 0, hi = nums.length - 1;
+        while(lo <= hi){
+            int mid = lo + (hi - lo)/2;
+
+            if(nums[mid] < key){
+               lo = mid+1;
+            } else if(nums[mid] > key){
+                hi = mid -1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
--- src/main/java/practices/recursion/simple/ReverseNumbers.java
+++ src/main/java/practices/recursion/simple/ReverseNumbers.java
@@ -1,13 +1,42 @@
 package practices.recursion.simple;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class ReverseNumbers {
 
     public static void main(String[] args) {
-        System.out.println(reverse(1234));
+        //  System.out.println(reverse(1234));
     }
 
+    public int countDistinctIntegers(int[] nums) {
+        int n = nums.length;
+        Set<Integer> set = new HashSet<>();
+        int[] doubled = new int[2 * n];
 
-    static int reverse(int n) {
+         for(int i = 0; i < n; i++){
+             doubled[i] = nums[i];
+             doubled[n+i] = reverse(nums[i]);
+         }
+
+        for (int j : doubled) {
+            set.add(j);
+        }
+         return set.size();
+    }
+
+    private int reverse(int x) {
+        int ans = 0;
+        while (x != 0) {
+            int rem = x % 10;
+            ans = ans * 10 + rem;
+            x = x / 10;
+        }
+        return ans;
+    }
+
+    static int reversee(int n) {
         int digits = (int) (Math.log10(n)) + 1; //?
         return helper(n, digits);
     }
@@ -17,6 +46,6 @@ public class ReverseNumbers {
             return n;
         }
         int rem = n % 10;
-        return rem * (int)(Math.pow(10, digits-1)) + helper(n/10, digits-1);
+        return rem * (int) (Math.pow(10, digits - 1)) + helper(n / 10, digits - 1);
     }
 }
