14b0e201654b6d0077c6ee05e664173b2779dbbc
Gulzhasm <gulzhasm@gmail.com>
2024-01-24 11:13:55 +0600

    sliding window

--- src/main/java/dsa/backtracking/Solution.java
+++ src/main/java/dsa/backtracking/Solution.java
@@ -8,10 +8,51 @@ import java.util.stream.Collectors;
 
 public class Solution {
     public static void main(String[] args) {
+        int[] arr = {1,12,-5,-6,50,3};
+        System.out.println(findMaxAverage(arr, 4));
 
-        String s = "aababcabc";
-        System.out.println(countGoodSubstrings(s));
+    }
 
+    public static  double findMaxAverage(int[] nums, int k) {
+        long sum = 0;
+        for (int i = 0; i < k; i++){
+            sum += nums[i];
+        }
+        long max = sum;
+
+        for (int i = k; i < nums.length; i++) {
+            sum += nums[i] - nums[i - k];
+            max = Math.max(max, sum);
+        }
+
+        return max / 1.0 / k;
+    }
+
+    public static int numberOfSubarrays(int[] nums, int k) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 == 0) {
+                nums[i] = 0;
+            } else {
+                nums[i] = 1;
+            }
+        }
+
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            int sum = 0;
+            int j = i;
+            while (j < nums.length) {
+                sum += nums[j];
+                j++;
+                if(sum == k){
+                    count++;
+                    break;
+                }
+            }
+
+        }
+        return count;
     }
 
     public static int countGoodSubstrings(String s) {
