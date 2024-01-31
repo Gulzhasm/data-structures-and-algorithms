f4c11127ba22d89bb6e240e3b097b5bae5b9ea72
Gulzhasm <gulzhasm@gmail.com>
2024-01-31 16:33:28 +0600

    lc - jan31

--- src/main/java/leetcode/challenges/january/Jan31.java
+++ src/main/java/leetcode/challenges/january/Jan31.java
@@ -1,37 +1,82 @@
 package leetcode.challenges.january;
 
 
+import java.util.Arrays;
 import java.util.Stack;
 
 public class Jan31 {
 
-    public int[] dailyTemperatures(int[] temperatures) {
+
+    //Input: temperatures = [73,74,75,71,69,72,76,73]
+    //Output: [1,1,4,2,1,1,0,0]
+    public static int[] dailyTemperatures(int[] temperatures) {
         int N = temperatures.length;
         int[] ans = new int[N];
         Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < N; i++) {
+            while (!stack.empty() && temperatures[stack.peek()] < temperatures[i]) {
+                ans[stack.peek()] = i - stack.pop();
+            }
+            stack.push(i);
+        }
+        return ans;
 
-        stack.add(temperatures[0]);
-        ans[0] = 1;
+    }
+    public static void main(String[] args) {
+        int[] temperatures = {1,3,5,4,7};
+        System.out.println(findLengthOfLCIS(temperatures));
+        //[3,1,1,2,1,1,0,1,1,0]
+    }
+    //Input: nums = [1,3,5,4,7]
+    //Output: 3
+    public static int findLengthOfLCIS(int[] nums) {
+        int count = 0;
+        int max = 0;
+         for(int i = 1; i < nums.length; i++){
+             if(nums[i-1] < nums[i]){
+                 count++;
+                 max = Math.max(count, max);
+             } else {
+                 count = 0;
+             }
+         }
+
+        return max + 1;
+    }
+
+    public static int[] dailyTemperaturesNaive(int[] temperatures) { //works - but the last test case - Time Limit Exceeded -
+        int N = temperatures.length;
+        int[] ans = new int[N];
         int j = 0;
+
         for (int i = 1; i < N; i++) {
-            int curr = stack.pop();
-            if (curr < temperatures[i]){
+            int prev = temperatures[j];
+            int curr = temperatures[i];
+            if (prev < curr) {
                 ans[j] = 1;
+            } else if (prev > curr && i == N - 1) {
+                ans[j] = 0;
             } else {
-                int count =0;
-                int k = temperatures[i];
-                while(curr > k ){
-                    k++;
-                    count++;
+                int count = 0;
+                int k = i;
+                while (k < N) {
+                    if (prev >= temperatures[k]) {
+                        if (k == N - 1) {
+                            count = 0;
+                            break;
+                        }
+                        count++;
+                        k++;
+
+                    } else {
+                        count++;
+                        break;
+                    }
                 }
                 ans[j] = count;
             }
             j++;
-            stack.add(temperatures[i]);
         }
-
-
         return ans;
-
     }
 }
