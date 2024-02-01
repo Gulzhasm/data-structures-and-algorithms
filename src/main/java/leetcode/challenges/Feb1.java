0e69cb04ca8db6e169b4b83aac45b97164007e8d
Gulzhasm <gulzhasm@gmail.com>
2024-02-01 16:56:03 +0600

    daily challenge

---
+++ src/main/java/leetcode/challenges/Feb1.java
@@ -0,0 +1,73 @@
+package leetcode.challenges;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class Feb1 {
+
+//Input: s = "ab#c", t = "ad#c"
+//Output: true
+//Explanation: Both s and t become "ac".
+public static void main(String[] args) {
+    System.out.println(backspaceCompare("ab##", "c#d#"));
+
+}
+    public static boolean backspaceCompare(String s, String t) {
+        Stack<Character> sStack = new Stack<>();
+        Stack<Character> tStack = new Stack<>();
+
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '#'){
+                if(!sStack.isEmpty()) {
+                    sStack.pop();
+                }
+            } else if(s.charAt(i) != '#') {
+                sStack.add(s.charAt(i));
+            }
+        }
+
+        for(int i = 0; i < t.length(); i++){
+            if(t.charAt(i) == '#'){
+                if(!tStack.isEmpty()) {
+                    tStack.pop();
+                }
+            } else  if(t.charAt(i) != '#') {
+                tStack.add(t.charAt(i));
+            }
+        }
+        if(sStack.size() != tStack.size()){
+            return false;
+        } else {
+            for (int i = 0; i < sStack.size(); i++) {
+                if(sStack.get(i) != tStack.get(i)){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+
+
+
+    //Input: nums = [1,3,4,8,7,9,3,5,1], k = 2
+//Output: [[1,1,3],
+//         [3,4,5],
+//         [7,8,9]]
+//Explanation: We can divide the array into the following arrays: [1,1,3], [3,4,5] and [7,8,9].
+//The difference between any two elements in each array is less than or equal to 2.
+//Note that the order of elements is not important.
+    public int[][] divideArray(int[] nums, int k) {
+        int N = nums.length;
+        int[][] ans = new int[N / 3][3];
+        Arrays.sort(nums);
+
+        for (int i = 2; i < N; i += 3) {
+            if (nums[i - 2] - nums[i - 1] > k) {
+                return new int[N / 3][];
+            }
+            ans[i / 3] = new int[]{nums[i - 2], nums[i - 1], nums[i]};
+        }
+        return ans;
+    }
+}
--- src/main/java/leetcode/challenges/january/Jan31.java
+++ src/main/java/leetcode/challenges/january/Jan31.java
@@ -1,11 +1,35 @@
 package leetcode.challenges.january;
 
 
-import java.util.Arrays;
-import java.util.Stack;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Jan31 {
 
+    public int[] KWeakestRows(int[][] mat, int k) {
+        int[] ans = new int[k];
+        Map<Integer, Integer> rows = new HashMap<>();
+
+        for (int i = 0; i < mat.length; i++) {
+            int count = 0;
+            for (int j = 0; j < mat[i].length; j++) {
+                if (mat[i][j] == 1) {
+                    count++;
+                }
+            }
+            rows.put(i, count);
+        }
+        List<Integer> list = rows.entrySet().stream()
+                .sorted(Map.Entry.comparingByValue())
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+
+
+        for (int i = 0; i < k; i++) {
+            ans[i] = list.get(i);
+        }
+        return ans;
+    }
 
     //Input: temperatures = [73,74,75,71,69,72,76,73]
     //Output: [1,1,4,2,1,1,0,0]
@@ -22,24 +46,26 @@ public class Jan31 {
         return ans;
 
     }
+
     public static void main(String[] args) {
-        int[] temperatures = {1,3,5,4,7};
+        int[] temperatures = {1, 3, 5, 4, 7};
         System.out.println(findLengthOfLCIS(temperatures));
         //[3,1,1,2,1,1,0,1,1,0]
     }
+
     //Input: nums = [1,3,5,4,7]
     //Output: 3
     public static int findLengthOfLCIS(int[] nums) {
         int count = 0;
         int max = 0;
-         for(int i = 1; i < nums.length; i++){
-             if(nums[i-1] < nums[i]){
-                 count++;
-                 max = Math.max(count, max);
-             } else {
-                 count = 0;
-             }
-         }
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i - 1] < nums[i]) {
+                count++;
+                max = Math.max(count, max);
+            } else {
+                count = 0;
+            }
+        }
 
         return max + 1;
     }
