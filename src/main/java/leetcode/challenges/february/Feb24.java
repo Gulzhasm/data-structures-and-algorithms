297f2122d2960780c3110d62dbcb06bb8ba1b4b0
Gulzhasm <gulzhasm@gmail.com>
2024-02-25 16:36:06 +0600

    lc

---
+++ src/main/java/leetcode/challenges/february/Feb24.java
@@ -0,0 +1,34 @@
+package leetcode.challenges.february;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Feb24 {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        Map<Character, Integer> mapM = new HashMap<>();
+
+        for(char m: magazine.toCharArray()){
+            mapM.put(m, mapM.getOrDefault(m, 0)+1);
+        }
+
+       for(char c : ransomNote.toCharArray()) {
+           if (mapM.containsKey(c) && mapM.get(c) > 0) {
+               mapM.put(c, mapM.get(c) - 1);
+           } else return false;
+       }
+           return true;
+    }
+
+    public String reverseWords(String s) {
+        //array with split
+        //use string builder and append
+        String[] arr = s.trim().split("\\s+");
+        StringBuilder sb = new StringBuilder();
+        for(int i = arr.length-1; i >=0; i--){
+            sb.append(arr[i] + " ");
+        }
+
+        return sb.toString().trim();
+    }
+
+}
