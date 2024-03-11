9776f0df70ea96f76934f43596975073f4585dc1
Gulzhasm <gulzhasm@gmail.com>
2024-03-11 10:38:37 +0500

    lc - strings

--- src/main/java/leetcode/topicWise/Strings.java
+++ src/main/java/leetcode/topicWise/Strings.java
@@ -26,4 +26,23 @@ public class Strings {
         return sb.toString();
 
     }
+
+    public int numJewelsInStones(String jewels, String stones) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        for(char s : stones.toCharArray()){
+            map.put(s, map.getOrDefault(s,0)+1);
+        }
+
+        int ans = 0;
+        for(char j: jewels.toCharArray()){
+            if(map.containsKey(j)){
+                int c = map.get(j);
+                ans+=c;
+            }
+
+        }
+
+        return ans;
+    }
 }
