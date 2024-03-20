2343ec3381525de1d60450ee10f81945d720ef20
Gulzhasm <gulzhasm@gmail.com>
2024-03-20 13:37:24 +0500

    lc

--- src/main/java/leetcode/topicWise/linkedList/Solution.java
+++ src/main/java/leetcode/topicWise/linkedList/Solution.java
@@ -20,6 +20,19 @@ class ListNode {
 
 class Solution {
 
+        public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
+            ListNode end = list1, start = null;
+            for(int i = 0; i < b; i++, end = end.next){
+                if(i == a -1) start = end;
+            }
+            start.next = list2;
+
+            while(list2.next!=null) list2 = list2.next;
+            list2.next = end.next;
+            end.next = null;
+            return list1;
+        }
+
 
     public ListNode middleNode(ListNode head) {
         ListNode slow = head;
