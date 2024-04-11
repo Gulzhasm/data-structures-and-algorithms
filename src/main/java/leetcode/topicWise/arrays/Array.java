46fbebe6b9892fa3e2b25e7f8f784718c6014e7f
Gulzhasm <gulzhasm@gmail.com>
2024-04-11 23:27:40 +0500

    stack example

--- src/main/java/leetcode/topicWise/arrays/Array.java
+++ src/main/java/leetcode/topicWise/arrays/Array.java
@@ -90,6 +90,34 @@ public class Array {
     public static void main(String[] args) {
         topKFrequent(new int[]{1,1,1,2,2,3}, 2);
     }
+    public String removeKdigits(String num, int k) {
+        if(k >= num.length()) return "0";
+
+        Deque<Character> stack = new ArrayDeque<>();
+        for(char c : num.toCharArray()) {
+            while(k > 0 && !stack.isEmpty() && stack.peekLast() > c) {
+                stack.removeLast();
+                k--;
+            }
+            stack.addLast(c);
+        }
+
+        while(k>0) {
+            stack.removeLast();
+            k--;
+        }
+
+        // Remove all zeros from the front of the stack and then if stack is empty, return "0"
+        while(!stack.isEmpty() && stack.peekFirst()== '0') stack.removeFirst();
+        if(stack.isEmpty()) return "0";
+
+        // build the number from the stack
+        StringBuilder sb = new StringBuilder();
+        while(!stack.isEmpty()) {
+            sb.append(stack.removeFirst());
+        }
+        return sb.toString();
+    }
 
     //Input: nums = [1,1,1,2,2,3], k = 2 => 1=3, 2 =2,
     //Output: [1,2]
