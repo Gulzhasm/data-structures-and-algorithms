548f52afec351ddb6ffe0a518c89663396f2460f
GULZHAS MAILYBAYEVA <gulzhasm@gmail.com>
2024-04-15 21:12:15 +0500

    bitwise

--- src/main/java/leetcode/topicWise/arrays/base/Misc.java
+++ src/main/java/leetcode/topicWise/arrays/base/Misc.java
@@ -34,4 +34,15 @@ public class Misc {
 
         return ans;
     }
+
+
+    public int hammingWeight(int n) {
+        int mask = 1;
+        int bits = 0;
+        for(int i = 1; i <=32; i++) {
+            bits+= (n & mask) == 0 ? 0 : 1;
+            mask<<=1;
+        }
+        return bits;
+    }
 }
