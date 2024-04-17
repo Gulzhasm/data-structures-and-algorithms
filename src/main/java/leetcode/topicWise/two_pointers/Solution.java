428a60996e11994b133ca22356a70f60800d2a47
GULZHAS MAILYBAYEVA <v-G.Mailybayeva@lionbridge.com>
2024-04-17 20:17:34 +0500

    recap

---
+++ src/main/java/leetcode/topicWise/two_pointers/Solution.java
@@ -0,0 +1,18 @@
+package leetcode.topicWise.two_pointers;
+
+public class Solution {
+    //Input: s = "abc", t = "ahbgdc"
+    //Output: true
+    public boolean isSubsequence(String s, String t) {
+        int i = 0, j = 0;
+
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+            }
+            j++;
+        }
+
+        return i == s.length();
+
+    }
