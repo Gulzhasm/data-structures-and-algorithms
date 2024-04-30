8c4bd15b75a480cb061e0e8497ad3946d6e7ab5b
Gulzhasm <gulzhasm@gmail.com>
2024-04-30 20:33:56 +0500

    btiwise

--- src/main/java/leetcode/topicWise/strings/Review.java
+++ src/main/java/leetcode/topicWise/strings/Review.java
@@ -7,6 +7,35 @@ public class Review {
         System.out.println(isinOrder("bca"));
     }
 
+    //Example 1:
+    //
+    //Input: word = "aba"
+    //Output: 4
+    //Explanation: The four wonderful substrings are underlined below:
+    //- "aba" -> "a"
+    //- "aba" -> "b"
+    //- "aba" -> "a"
+    //- "aba" -> "aba"
+
+    public long wonderfulSubstrings(String word) {
+        long[] count = new long[1024]; // 2^10 to store XOR values
+        long result = 0;
+        int prefixXor = 0;
+        count[prefixXor] = 1;
+
+        for (char ch : word.toCharArray()) {
+            int charIndex = ch - 'a';
+            prefixXor ^= 1 << charIndex;
+            result += count[prefixXor];
+            for (int i = 0; i < 10; i++) {
+                result += count[prefixXor ^ (1 << i)];
+            }
+            count[prefixXor]++;
+        }
+
+        return result;
+    }
+
     public int minOperations(int[] A, int k) {
         for (int a : A)
             k ^= a;
