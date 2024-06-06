2c5440470a3433d1a22c8d9193c1e5f528676552
GULZHAS MAILYBAYEVA <v-G.Mailybayeva@lionbridge.com>
2024-06-06 19:38:37 +0500

    hashmap

--- src/main/java/leetcode/june/Strings.java
+++ src/main/java/leetcode/june/Strings.java
@@ -1,11 +1,33 @@
 package leetcode.june;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Strings {
+
+
+    public boolean isNStraightHand(int[] hand, int groupSize) {
+        if (hand.length % groupSize != 0) return false;
+
+
+        Map<Integer, Integer> count = new HashMap<>();
+        for (int card : hand) {
+            count.put(card, count.getOrDefault(card, 0) + 1);
+        }
+
+        // Iterate over the map
+        for (int key : count.keySet()) {
+            if (count.get(key) > 0) {
+                for (int i = 1; i < groupSize; i++) {
+                    if (count.getOrDefault(key + i, 0) < count.get(key)) {
+                        return false;
+                    }
+                    count.put(key + i, count.get(key + i) - count.get(key));
+                }
+            }
+        }
+        return true;
+    }
+
 //Input: words = ["bella","label","roller"]
 //Output: ["e","l","l"]
 
